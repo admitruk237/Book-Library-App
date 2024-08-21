0cec384c8e8d3c7d790b2e765f0efb9da8dd8b91
Andrii Dmytruk <admitruk237@gmail.com>
2024-08-21 15:42:23 +0200

    Book form

---
+++ frontent/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import BookList from './components/BookList/BookList';
+import Filter from './components/Filter/Filter';
+import BookForm from './components/BookForm/BookForm';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Book Libary App</h1>
+      </header>
+      <main className="app-main">
+        <div className="app-left-column">
+          <BookForm />
+        </div>
+        <div className="app-right-column">
+          {/*   <Filter /> */}
+          <BookList />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
