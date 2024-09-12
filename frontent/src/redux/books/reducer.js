7f20940558abb0e23dc9e7242678932218cf7f91
Andrii Dmytruk <admitruk237@gmail.com>
2024-09-12 14:20:01 +0200

    Added delete button

--- frontent/src/redux/books/reducer.js
+++ frontent/src/redux/books/reducer.js
@@ -6,10 +6,10 @@ const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case a.ADD_BOOK:
       return [...state, action.payload];
-
+    case a.DELETE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
     default:
       return state;
   }
 };
-
 export default booksReducer;
